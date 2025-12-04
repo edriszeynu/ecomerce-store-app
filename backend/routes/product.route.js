457f4c5683d87de81dc91712ade4502a15ed2375
edris zeynu <endriszeynu173@gmail.com>
2025-12-04 13:50:03 +0300

    add functionality of auth

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
-import { getAllProducts } from '../controllers/product.controller.js';
+import { getAllProducts, getFeaturedProduct } from '../controllers/product.controller.js';
 
 const router=express.Router();
 
 router.get("/",protectRoute,adminRoute,getAllProducts);
 
 router.get("/featured",getFeaturedProduct)
+router.post("/",protectRoute,adminRoute,createProduct);
 
 
 export default router;
+dunycejwk
