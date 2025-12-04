39c03a4b03545a7a97c33f389e5c8867bcd0d6de
edris zeynu <endriszeynu173@gmail.com>
2025-12-04 14:57:38 +0300

    add delete product functionality

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
-import { getAllProducts, getFeaturedProduct } from '../controllers/product.controller.js';
+import { createProduct, getAllProducts, getFeaturedProduct } from '../controllers/product.controller.js';
 
 const router=express.Router();
 
@@ -8,6 +8,7 @@ router.get("/",protectRoute,adminRoute,getAllProducts);
 
 router.get("/featured",getFeaturedProduct)
 router.post("/",protectRoute,adminRoute,createProduct);
+router.delete("/:id",protectRoute,adminRoute,deleteProduct);
 
 
 export default router;
